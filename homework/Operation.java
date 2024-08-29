35715c20b2d0d44e94834d5e57cf71ec160666dc
Neyyadupakkam Sundarasekaran <ngsundar@utexas.edu>
2024-08-29 14:51:05 -0700

    Add files via upload
    uploaded from eclipse project

---
+++ homework/Operation.java
@@ -0,0 +1,33 @@
+package com.synopsis.homework;
+
+public enum Operation {
+	ADD{
+		@Override
+		public Number compute(Number num1, Number num2) {
+			return num1.doubleValue() + num2.doubleValue();
+		}
+	},
+	SUBTRACT{
+		@Override
+		public Number compute(Number num1, Number num2) {
+			return num1.doubleValue() - num2.doubleValue();
+		}
+	},
+	MULTIPLY{
+		@Override
+		public Number compute(Number num1, Number num2) {
+			return num1.doubleValue() * num2.doubleValue();
+		}
+	},
+	DIVIDE{
+		@Override
+		public Number compute(Number num1, Number num2) {
+			if(num2.doubleValue() == 0.0) {
+				throw new ArithmeticException("Divide by zero");
+			}
+			return num1.doubleValue() / num2.doubleValue();
+		}
+	};
+	
+	public abstract Number compute(Number num1, Number num2);
+}
